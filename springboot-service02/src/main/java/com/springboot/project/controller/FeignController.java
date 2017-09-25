78283da38eae2cc4919bce02b40a1c2572d57a22
fengli <1473357859@qq.com>
2017-09-25 17:15:28 +0800

    集成swagger2的修改： 关于传递参数的不同的，swagger2的配置不一样。

--- springboot-service02/src/main/java/com/springboot/project/controller/FeignController.java
+++ springboot-service02/src/main/java/com/springboot/project/controller/FeignController.java
@@ -3,12 +3,10 @@ package com.springboot.project.controller;
 import com.springboot.project.entity.Student;
 import com.springboot.project.service.FeignService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Administrator on 2017/9/25.
@@ -16,19 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api("feignController相关的api")
 public class FeignController {
-
+    //这里如果是用@PathVariable来获取参数的时候，可以用swagger的注解@ApiImplicitParam
+    //如果是用@RequestParam来获取参数，则不要用swagger的这个注解
     @Autowired
     private FeignService feignService;
     @ApiOperation(value = "服务的消费者调用服务提供者提供的接口", notes = "调用服务的提供者")
-   // @ApiImplicitParam(name = "name", value = "姓名", paramType = "path", required = true, dataType = "String")
-    @RequestMapping(value = "hi",method = RequestMethod.GET)
-    public String feignCall(@RequestParam(value ="name") String name){
+    @ApiImplicitParam(name = "name", value = "姓名", paramType = "path", required = true, dataType = "String")
+    @RequestMapping(value = "hi/{name}",method = RequestMethod.GET)
+    public String feignCall(@PathVariable String name){
      return    feignService.sayHiFromClientOne(name);
     }
     @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个的学生信息")
-   // @ApiImplicitParam(name = "id", value = "学生ID", paramType = "path", required = true, dataType = "Integer")
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public Student feignCall2(@RequestParam(value = "id") Integer id){
+    @ApiImplicitParam(name = "id", value = "学生ID", paramType = "path", required = true, dataType = "Integer")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Student feignCall2(@PathVariable Integer id){
         return feignService.sayHiFromClientTwo(id);
 
     }
