f253b64ddc51053f34d61911e7c1e562ebf7c6f1
fengli <1473357859@qq.com>
2017-09-27 16:52:23 +0800

    断路器的配置

--- springboot-service02/src/main/java/com/springboot/project/serviceImpl/FeignService02Hystrix.java
+++ springboot-service02/src/main/java/com/springboot/project/serviceImpl/FeignService02Hystrix.java
@@ -1,21 +1,21 @@
 package com.springboot.project.serviceImpl;
 
 import com.springboot.project.entity.Student;
-import com.springboot.project.service.FeignService;
+import com.springboot.project.service.FeignService02;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by Administrator on 2017/9/25.
+ * 短路器的实现
  */
 @Component
-public class FeignSerivceHystric implements FeignService {
+public class FeignService02Hystrix implements FeignService02 {
     @Override
     public String sayHiFromClientOne(String name) {
-        return "sorry"+name;
+        return "SORRY"+name;
     }
 
     @Override
     public Student sayHiFromClientTwo(Integer id) {
-        return new Student(null);
+        return new Student(1);
     }
 }
