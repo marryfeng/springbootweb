7ee6a29ddaa9221ebfb5f66aff2cb450a6269827
fengli <1473357859@qq.com>
2017-10-12 20:47:49 +0800

    springboot整合文件上传

---
+++ springboot-service01/src/main/java/com/springboot/project/controller/FileUploadController.java
@@ -0,0 +1,43 @@
+package com.springboot.project.controller;
+
+import com.springboot.project.Utils.FileUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 上传文件的类
+ */
+@Controller
+public class FileUploadController {
+
+    @RequestMapping(value="/gouploadimg", method = RequestMethod.GET)
+    public String goUploadImg() {
+        //跳转到 templates 目录下的 uploadimg.html
+        return "uploadimg";
+    }
+
+    //处理文件上传
+    @RequestMapping(value="/testuploadimg", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadImg(@RequestParam("file") MultipartFile file,
+                     HttpServletRequest request) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        /*System.out.println("fileName-->" + fileName);
+        System.out.println("getContentType-->" + contentType);*/
+        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+        try {
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        return "uploadimg success";
+    }
+}
