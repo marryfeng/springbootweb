5867244d19530db28d04b8e2281d8e52b30977f1
fengli <1473357859@qq.com>
2017-10-12 20:48:33 +0800

    springboot整合文件上传的工具类

---
+++ springboot-service01/src/main/java/com/springboot/project/Utils/FileUtil.java
@@ -0,0 +1,22 @@
+package com.springboot.project.Utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * 文件上传的工具类
+ */
+public class FileUtil {
+
+    public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception {
+        File targetFile = new File(filePath);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        FileOutputStream out = new FileOutputStream(filePath+fileName);
+        out.write(file);
+        out.flush();
+        out.close();
+    }
+
+}
